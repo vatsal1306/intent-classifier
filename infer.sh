21799a1a7bc7576fac9002519dcc8d435cc1bc16
vatsal-aimonk <vatsal@aimonk.com>
2025-12-18 01:14:33 +0530

    Add inference script for misclassified and unlabelled data predictions

---
+++ infer.sh
@@ -0,0 +1,13 @@
+# list all incorrect predictions
+python -m src.inference \
+--ckpt runs/first_run_epochs/best_model \
+--data-csv dataset/splits/test.csv \
+--out-csv runs/first_run_epochs/misclassified_test.csv \
+--labeled
+
+
+# inference on unlabelled infer data
+python -m src.inference \
+--ckpt runs/first_run_epochs/best_model \
+--data-csv dataset/processed/infer_data.csv \
+--out-csv runs/first_run_epochs/infer_data.csv
